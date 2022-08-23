be50968e989b001ce7923a5482b6e5043e4adf0b
Kyle Parker <kyle.z.parker3@gmail.com>
2022-08-23 16:50:03 -0700

    pre class 4

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,30 @@
 from django.views.generic import TemplateView
 
+
+
+from bs4 import BeautifulSoup 
+import requests
+
+URL = "https://wttr.in"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+}
+
+
 class HomePageView(TemplateView):
     template_name="pages/home.html"
+
+    def get(self, request, *args, **kwargs):
+        context= self.get_context_data(**kwargs)
+        resp = requests.get(URL, headers=HEADERS)
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.text, "html.parser")
+            weather_pre = soup.find("pre")
+        else:
+            weather_pre = "%s" % resp.text
+        context["weather"] =str(weather_pre)
+        return self.render_to_response(context)
 
 class AboutPageView(TemplateView):
     template_name="pages/about.html"
